0af9a593be06a9a0929426dd71dc3adff477a734
appleicat <appleicat@outlook.com>
2024-05-11 16:40:37 +0400

    update website

--- src/pages/_page.jsx
+++ src/pages/_page.jsx
@@ -22,8 +22,8 @@ export default function Page() {
   const [data, setData] = useState('');
   const [camera, setCamera] = useState(false);
   const [errorCorrectionLevel, setErrorCorrectionLevel] = useState('L');
-  // const [maskPattern, setMaskPattern] = useState(undefined);
-  // const [version, setVersion] = useState(undefined);
+  const [maskPattern, setMaskPattern] = useState(undefined);
+  const [version, setVersion] = useState(undefined);
 
   useEffect(() => {
     if (data) setCamera(false);
@@ -45,8 +45,8 @@ export default function Page() {
         margin: 0,
         color: { dark: '000000FF', light: '00000000' },
         errorCorrectionLevel,
-        // maskPattern,
-        // version,
+        maskPattern,
+        version,
       },
       (err, qrc) => {
         err ? console.log(err) : (qrRef.current.innerHTML = qrc);
@@ -122,7 +122,7 @@ export default function Page() {
             <div className="grid grid-flow-row-dense w-full h-full items-end" />
             <div className="grid grid-flow-row-dense w-full h-full items-end" />
             <div className="grid grid-flow-row-dense w-full h-full items-end" />
-            {/* <div className="grid grid-flow-row-dense w-full h-full items-end">
+            <div className="grid grid-flow-row-dense w-full h-full items-end">
               <div>version</div>
               <div className="grid grid-cols-10 w-full h-full">
                 <div className="col-span-full">
@@ -188,8 +188,8 @@ export default function Page() {
                   </Button>
                 ))}
               </div>
-            </div> */}
-            {/* <div className="grid grid-flow-row-dense w-full h-full items-end">
+            </div>
+            <div className="grid grid-flow-row-dense w-full h-full items-end">
               <div>mask pattern</div>
               <div className="grid grid-cols-8 w-full h-full">
                 <div className="col-span-full">
@@ -214,7 +214,7 @@ export default function Page() {
                   </Button>
                 ))}
               </div>
-            </div> */}
+            </div>
             <div className="grid grid-flow-row-dense w-full h-full items-end">
               <div>error correction</div>
               <div className="grid grid-flow-col-dense w-full h-full">
